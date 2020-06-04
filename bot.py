d2fa14fa72bef8e314afba8d90e615f1a105001a
cholland4 <cameron_holland@sbcglobal.net>
2020-06-03 19:03:43 -0700

    updated token

--- bot.py
+++ bot.py
@@ -460,4 +460,4 @@ async def flip(ctx):
                 await ctx.send("Tails!")
 
 
-client.run("NzA3NzI0OTUzMzUyNTM2MTU2.XtMTXQ.ibFU_ubrKjpxi4LTKMnLnDeka2w")
+client.run("NzA3NzI0OTUzMzUyNTM2MTU2.XtS5MQ.N35CrNJ56axgzpasp640Pi3oS6w")
