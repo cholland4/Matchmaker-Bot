d4694af283dccf04173626c845ffe484b06d2e42
Cameron Holland <49530465+cholland4@users.noreply.github.com>
2020-11-09 10:35:51 -0800

    renamed variables for consistency

--- bot_classes.py
+++ bot_classes.py
@@ -1,15 +1,44 @@
 class Server:
     def __init__(self, server_id, server_name):
         self.server_id = server_id
-        self.server_name = server_name
+        self.name = server_name
         self.draft_channel = None
-        self.player_queue = []      # a list of player ID's, lookup their information in the dictionary by ID
+        self.player_queue = {"tank": [],
+                             "dps": [],
+                             "support": []}
         self.games_in_progress = []  # append each game object as the mm makes them, and remove each game obj
         #                              as a win is reported
 
     def __str__(self):
-        output = self.server_name
+        output = self.name
         return output
+
+    def get_full_queue(self):
+        full_queue = []
+        for discord_id in self.player_queue["tank"]:
+            full_queue.append(discord_id)
+        for discord_id in self.player_queue["dps"]:
+            full_queue.append(discord_id)
+        for discord_id in self.player_queue["support"]:
+            full_queue.append(discord_id)
+
+        print("tank:", self.player_queue["tank"])
+        print("dps:", self.player_queue["dps"])
+        print("supp:", self.player_queue["support"])
+        return full_queue
+
+    def remove_from_queue(self, discord_id):
+        if discord_id in self.player_queue["tank"]:
+            self.player_queue["tank"].remove(discord_id)
+        if discord_id in self.player_queue["dps"]:
+            self.player_queue["dps"].remove(discord_id)
+        if discord_id in self.player_queue["support"]:
+            self.player_queue["support"].remove(discord_id)
+
+    def clear_queue(self):
+        self.player_queue = {"tank": [],
+                             "dps": [],
+                             "support": []}
 
 
 class User:
